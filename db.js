d9168f0cb8d5fd56e79be1f9d5d5c1030828ed7c
oheunbee <dhdmsql1324@gmail.com>
2022-06-22 16:10:07 +0900

    rtest

---
+++ db.js
@@ -0,0 +1,65 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  port : '3306',
+  database : 'memo',
+  dateStrings : 'date' //날짜 시간 출력
+});
+ 
+
+
+//리스트 전체를 불러오는 함수
+function getAllMemos(callback) {
+    connection.query('select * from memos ORDER BY id DESC' , 
+    (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+//리스트에 새로운 내용을 추가하는 함수
+function insertMemo(content, title,writer,callback) {
+    connection.query(`INSERT INTO memos(content, created, updated, title, writer) VALUES('${content}',NOW(), NOW(),'${title}','${writer}')`,(err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+//리스트 중 id값이 일치하는 row만 불러오는 방법
+function getMemoById(id, callback) {
+    connection.query(`SELECT * from memos WHERE ID = ${id}` , (err, row, fields) => {
+        if(err) throw err;
+        callback(row);
+    });
+}
+
+//리스트를 수정하고 싶을 때 id값이 일치하는 부분을 수정하는 함수
+function updateMemoById(id, content , title, writer,callback){
+    connection.query(`UPDATE MEMOS SET CONTENT='${content}', title='${title}',writer='${writer}', updated=NOW() WHERE ID='${id}'`, (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+
+//리스트 중 id값이 일치하는 부분을 삭제하는 함수
+function deleteMemoById(id, callback) {
+    connection.query(`DELETE from memos WHERE ID = ${id}` , (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+
+
+module.exports = {
+    getAllMemos,
+    insertMemo,
+    getMemoById,
+    updateMemoById,
+    deleteMemoById
+}
+
---
+++ routes/route.js
@@ -0,0 +1,104 @@
+const express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+const router = express.Router();
+
+const {check, validationResult} = require('express-validator');
+
+const db = require('./../db.js');
+
+router.use(expressLayouts);
+  //route routing
+  router.get('/', (req,res) => {
+  res.render('index');
+  //서문시장야시장의 메인페이지를 지정하세요
+});
+
+  router.get('/page2', (req,res) => {
+    res.render("page2");
+  });
+  router.get('/login', (req,res) => {
+    res.render("login");
+  });
+  router.get('/newMemo', (req,res) => {
+    res.render("notice_write");
+  });
+  router.get('/page3', (req,res) => {
+    db.getAllMemos((rows) => {
+      res.render('page3', { rows : rows })
+  })
+  });
+  router.get('/page4', (req,res) => {
+    let id = req.query.id;
+    db.getMemoById(id, (row) => {
+        if (typeof id === 'undefined' || row.length <= 0 )  {
+            res.status(404).json({error:'undefined memo'});
+        } else {
+            res.render('page4' , {row:row[0]})
+        }
+    })
+  });
+
+  router.get('/join', (req,res) => {
+    res.render("join");
+  });
+  
+
+
+
+router.post('/store',
+  check('content').isLength({ min: 1, max: 500 }),
+  function (req, res, next) {
+  let errs = validationResult(req);
+  console.log(errs);
+  if(errs['errors'].length > 0){ //화면에 에러 출력하기 위함
+    res.render('newMemo',{errs:errs['errors']});
+  } else{
+    let param = JSON.parse(JSON.stringify(req.body));
+    db.insertMemo(param['content'],param['title'],param['writer'],() =>{
+      res.redirect('page3');
+    });
+  }
+});
+
+router.get('/updateMemo', (req, res) => {
+    let id = req.query.id;
+
+    db.getMemoById(id, (row) => {
+        if (typeof id === 'undefined' || row.length <= 0 )  {
+            res.status(404).json({error:'undefined memo'});
+        } else {
+            res.render('updateMemo' , {row:row[0]})
+        }
+    })
+});
+
+router.post('/updateMemo',
+    [check('content').isByteLength({min : 1, max: 300})],
+    (req, res) => {
+        let errs = validationResult(req);
+        let param = JSON.parse(JSON.stringify(req.body));
+        let id = param['id'];
+        let content = param['content'];
+        let title = param['title'];
+        let writer = param['writer'];
+        if (errs['errors'].length>0) {
+            db.getMemoById(id, (row) => {
+                res.render('updateMemo', {row:row[0], errs:errs['errors']})
+            })
+        } else {
+            db.updateMemoById(id, content, title, writer, () => {
+                res.redirect('page3');
+            })
+        }
+    });
+    
+    router.get('/deleteMemo' , (req, res) => {
+        let id = req.query.id;
+        db.deleteMemoById(id, () => {
+            res.redirect('page3');
+        });
+    });
+
+
+
+  module.exports = router;
