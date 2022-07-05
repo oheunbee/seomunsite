55d3e8adfd6845bb21dce754861ca1ff1f969726
oheunbee <dhdmsql1324@gmail.com>
2022-07-05 15:57:24 +0900

    .....

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 Kakao.init('addc27a1d3c735ad9ea960785ec3d5f7');
 Kakao.isInitialized();
+document.getElementById('logout').style.display = 'none';
 
 
 function KakaoLogin() {
@@ -11,7 +12,6 @@ function KakaoLogin() {
                 console.log(response);
                 document.getElementById('user').innerText = response.Kakao_account.profile.nickname;
                 document.getElementById('login').style.display = 'none';
-                document.getElementById('logout').style.display = 'none';
                 alert( response.Kakao_account.profile.nickname + '님 로그인 되었습니다.')
             }        
         })
