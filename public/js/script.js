1b54af0157e43f09a9a41f588a25d4640e25ec78
oheunbee <dhdmsql1324@gmail.com>
2022-07-05 15:21:27 +0900

    0705

--- public/js/script.js
+++ public/js/script.js
@@ -8,12 +8,14 @@ function KakaoLogin() {
             url : '/v2/user/me',
             success : function (response){
                 console.log(response);
-                // document.getElementById('user').innerText = response.Kakao_account.profile.nickname;
+                document.getElementById('user').innerText = response.Kakao_account.profile.nickname;
+                document.getElementById('login').style.display = 'none';
+                alert( response.Kakao_account.profile.nickname + '님 로그인 되었습니다.')
             }        
         })
     }
    })
-}
+};
 
 function KakaoLogout() {
 
